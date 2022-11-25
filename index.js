02f2d4c85944452b07edaba21de8278d4100584c
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-11-25 11:08:35 +0600

    category collection added

--- index.js
+++ index.js
@@ -17,12 +17,49 @@ app.use(express.json());
 
 // Mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rzk36ti.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Step- 06 (jwt)
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 async function run(){
     try{
+        const productsCollection = client.db('EndlessEssentials').collection('products');
+        const categoryCollection = client.db('EndlessEssentials').collection('category');
 
+        // 
+        app.get('/products', async(req, res) => {
+            const query = {}
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        // 
+        app.get('/category/:name', async(req, res) => {
+            const name = req.params.name;
+            const filter = {name}
+            const result = await categoryCollection.find(filter).toArray();
+            res.send(result);
+        })
     }
     finally{
 
